474fe11fc3154e254920f78ab29ce236c06c00d3
Chetan Purkar <157473249+Chetan-Purkar@users.noreply.github.com>
2026-02-23 15:54:53 +0530

    change base url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,8 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
-    baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
