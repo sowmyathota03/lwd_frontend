64a0de11642a9c113603b9a8a8b0fe3915cfb9cc
sowmya <sowmyathota17@gmail.com>
2026-03-06 17:30:13 +0530

    add description in job cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,165 +1,110 @@
 import NavBar from './components/common/NavBar'
-import Footer from './components/common/Footer';
-import "./index.css";
-import { Routes,Route } from "react-router-dom"
+import Footer from './components/common/Footer'
+import "./index.css"
 
-import Login from './auth/Login';
-// import AuthSelection from './auth/AuthSelection';
-// import RegisterJobSeeker from './auth/RegisterJobSeeker';
-// import RegisterRecruiter from './auth/RegisterRecruiter';
-import Register from './auth/Register';
+import { Routes, Route } from "react-router-dom"
 
-import Home from './pages/public/Home';
-import Jobs from './pages/public/Jobs';
-import JobDetails from "./pages/public/JobDetails";
-import Companies from './pages/public/Companies';
-import Profile from './pages/profile/Profile';
-import Career from './pages/public/Career';
+import Login from './auth/Login'
+import Register from './auth/Register'
 
+import Home from './pages/public/Home'
+import Jobs from './pages/public/Jobs'
+import JobDetails from "./pages/public/JobDetails"
+import Companies from './pages/public/Companies'
+import Profile from './pages/profile/Profile'
+import Career from './pages/public/Career'
 
-import JobCard from "./components/jobs/JobCards";
-import JobSearchBlock from "./components/jobs/JobSearchBlock";
-import ResumeUpload from "./components/jobs/ResumeUpload";
-import JobAlert from "./components/jobs/JobAlert";
-import CareerAdvice from "./components/jobs/CareerAdvice";
-import ApplyJob from "./components/jobs/ApplyJob";
-import Suggestions from "./components/jobs/Suggestions";
-import MyApplications from "./components/profile/MyApplication/MyApplications";
-import SavedJobs from "./components/jobs/SavedJobs";
-import JobList from "./components/jobs/JobList";
+import JobCard from "./components/jobs/JobCards"
+import JobSearchBlock from "./components/jobs/JobSearchBlock"
+import ResumeUpload from "./components/jobs/ResumeUpload"
+import JobAlert from "./components/jobs/JobAlert"
+import CareerAdvice from "./components/jobs/CareerAdvice"
+import ApplyJob from "./components/jobs/ApplyJob"
+import Suggestions from "./components/jobs/Suggestions"
+import MyApplications from "./components/profile/MyApplication/MyApplications"
+import SavedJobs from "./components/jobs/SavedJobs"
+import JobList from "./components/jobs/JobList"
 
-import Education from "./components/profile/Education";
-import Internship from "./components/profile/Internship";
-import Project from "./components/profile/Project";
+import Education from './components/profile/Education'
+import Internship from './components/profile/Internship'
+import Project from './components/profile/Project'
+import AddStatus from './pages/profile/components/AddStatus'
+import VerifyEmail from './pages/profile/components/VerifyEmail'
 
-import AddStatus from "./pages/profile/components/AddStatus";
-import VerifyEmail from "./pages/profile/components/VerifyEmail";
+import ContactUs from './components/company/ContactUs'
+import AboutUs from './components/company/AboutUs'
+import PrivacyPolicy from './components/company/PrivacyPolicy'
+import TermsAndConditions from './components/company/TermsAndConditions'
+import Pricing from './components/company/Pricing'
 
+import AdminDashboard from './pages/admin/AdminDashboard'
+import AdminHome from './components/admin/AdminHome'
+import UserManagementPage from './components/admin/UserManagementPage'
+import CompanyManagementPage from './components/admin/CompanyManagementPage'
 
+import ManageJobs from './components/jobs/ManageJobs'
+import CreateJob from './components/jobs/CreateJob'
+import EditJob from './components/jobs/EditJob'
+import JobApplicationList from './components/jobApplications/JobApplicationList'
 
-import Education from './components/profile/Education';
-import Internship from './components/profile/Internship';
-import Project from './components/profile/Project';
-import AddStatus from './pages/profile/components/AddStatus';
-import VerifyEmail from './pages/profile/components/VerifyEmail';
+import RecruiterAdminDashboard from './pages/recruiter-admin/RecruiterAdminDashboard'
+import RecruiterAdminHome from './components/recruiter-admin/RecruiterAdminHome'
+import CompanyProfilePage from './components/recruiter-admin/CompanyProfilePage'
+import RecruiterList from './components/recruiter-admin/RecruiterList'
 
-import ContactUs from './components/company/ContactUs';
-import AboutUs from './components/company/AboutUs';
-import PrivacyPolicy from './components/company/PrivacyPolicy';
-import TermsAndConditions from './components/company/TermsAndConditions';
-import Pricing from './components/company/Pricing';
-
-
-import AdminDashboard from './pages/admin/AdminDashboard';
-import AdminHome from './components/admin/AdminHome';
-import UserManagementPage from './components/admin/UserManagementPage';
-import CompanyManagementPage from './components/admin/CompanyManagementPage';
-import ManageJobs from './components/jobs/ManageJobs';
-import CreateJob from './components/jobs/CreateJob';
-import EditJob from './components/jobs/EditJob';
-import JobApplicationList from './components/jobApplications/JobApplicationList';
-
-import RecruiterAdminDashboard from './pages/recruiter-admin/RecruiterAdminDashboard';
-import RecruiterAdminHome from './components/recruiter-admin/RecruiterAdminHome';
-import CompanyProfilePage from './components/recruiter-admin/CompanyProfilePage';
-import RecruiterList from './components/recruiter-admin/RecruiterList';
-
-
-
-
-
-import RecruiterDashboard from "./pages/recruiter/RecruiterDashboard";
-import RecruiterHome from "./components/recruiter/RecruiterHome";
-import RecruiterJobSeekerSearch from "./components/Searching/JobSeekerSearch/RecruiterJobSeekerSearch";
-import JobAnalytics from "./components/jobs/JobAnalytics";
-import RecruiterCompanyRequest from "./components/recruiter/RecruiterCompanyRequest";
-
+import RecruiterDashboard from "./pages/recruiter/RecruiterDashboard"
+import RecruiterHome from "./components/recruiter/RecruiterHome"
+import RecruiterJobSeekerSearch from "./components/Searching/JobSeekerSearch/RecruiterJobSeekerSearch"
+import JobAnalytics from "./components/jobs/JobAnalytics"
+import RecruiterCompanyRequest from "./components/recruiter/RecruiterCompanyRequest"
 
 function App() {
   return (
     <>
-    <div><NavBar/></div>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+      <NavBar />
 
-      
-      <Route path="/jobs" element={<Jobs />} />
-      <Route path="/jobs/:type" element={<Jobs />} />
-      <Route path="/job/:jobId" element={<JobDetails />} />
-      <Route path="/apply/:jobId" element={<ApplyJob />} />
-      <Route path="/JobCard" element={<JobCard/>}/>
-      <Route path="/JobSearchBlock" element={<JobSearchBlock/>}/>
-      <Route path="/my/applications" element={<MyApplications/>}/>
-      <Route path="/saved-jobs" element={<SavedJobs />} />
-      <Route path="/jobs-list" element={<JobList />} />
-      
-      
+      <Routes>
 
-      <Route path="/Companies" element={<Companies/>}/>
-      <Route path="/education" element={<Education/>}/>
-      <Route path="/internship" element={<Internship/>}/>
-      <Route path="/project" element={<Project/>}/>
-      <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
 
-      <Route path="/status" element={<AddStatus/>}/>
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/jobs/:type" element={<Jobs />} />
+        <Route path="/job/:jobId" element={<JobDetails />} />
+        <Route path="/apply/:jobId" element={<ApplyJob />} />
 
-      <Route path="/Career" element={<Career/>}/>     
-      <Route path="/profile" element={<Profile/>}/>
-      <Route path="/profile/:userId" element={<Profile/>}/>
-      <Route path="/ResumeUpload" element={<ResumeUpload/>}/>
-      <Route path="/JobAlert" element={<JobAlert/>}/>
-      <Route path="/CareerAdvice" element={<CareerAdvice/>}/>
-      <Route path="/ContactUs" element={<ContactUs/>}/>
-      <Route path="/AboutUs" element={<AboutUs/>}/>
-      <Route path="/PrivacyPolicy" element={<PrivacyPolicy/>}/>
-      <Route path="/TermsAndConditions" element={<TermsAndConditions/>}/>
-      <Route path="/Suggestions" element={<Suggestions/>}/>
-       <Route path="/pricing" element={<Pricing/>}/>
+        <Route path="/JobCard" element={<JobCard />} />
+        <Route path="/JobSearchBlock" element={<JobSearchBlock />} />
+        <Route path="/my/applications" element={<MyApplications />} />
+        <Route path="/saved-jobs" element={<SavedJobs />} />
+        <Route path="/jobs-list" element={<JobList />} />
 
-      
-      <Route path="/jobs/updatejob/:id" element={<EditJob />} />
+        <Route path="/companies" element={<Companies />} />
 
-      <Route path="/admin" element={<AdminDashboard />}>
-        <Route index element={<AdminHome />} />
-        <Route path="users" element={<UserManagementPage />} />
-        <Route path="companies" element={<CompanyManagementPage />} />
-        <Route e path=":companyId/companyprofile" element={<CompanyProfilePage />} />      
-        <Route path=":companyId/createjob" element={<CreateJob />}  />
-        <Route path="managejob" element={<ManageJobs />} />
-        <Route path="managejob/:jobId/analytics" element={<JobAnalytics />} />
-        <Route path="applications" element={<JobApplicationList />} />
-        <Route path="job-seekers" element={<RecruiterJobSeekerSearch />} />
-      </Route>
+        <Route path="/education" element={<Education />} />
+        <Route path="/internship" element={<Internship />} />
+        <Route path="/project" element={<Project />} />
 
-      <Route path="/recruiter-admin" element={<RecruiterAdminDashboard />} >
-        <Route index element={ <RecruiterAdminHome /> } />
-        <Route path="/recruiter-admin/company-profile" element={<CompanyProfilePage />} />
-        <Route path="/recruiter-admin/manage-recruiter" element={<RecruiterList />} />
-        <Route path="/recruiter-admin/managejob" element={<ManageJobs />} />
-        <Route path="/recruiter-admin/managejob/:jobId/analytics" element={<JobAnalytics />} />
-        <Route path="/recruiter-admin/createjob" element={<CreateJob />}  />
-        <Route path="/recruiter-admin/applications" element={<JobApplicationList />} />
-        <Route path="/recruiter-admin/job-seekers" element={<RecruiterJobSeekerSearch />} />
-      </Route>
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/status" element={<AddStatus />} />
 
-        <Route path="/recruiter/companyrequest" element={<RecruiterCompanyRequest />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/:userId" element={<Profile />} />
 
+        <Route path="/ResumeUpload" element={<ResumeUpload />} />
+        <Route path="/JobAlert" element={<JobAlert />} />
+        <Route path="/CareerAdvice" element={<CareerAdvice />} />
 
-      <Route path="/recruiter" element={<RecruiterDashboard />} >
-        <Route index element={ <RecruiterHome /> } />
-        <Route path="/recruiter/company-profile" element={<CompanyProfilePage />} />
-        <Route path="/recruiter/managejob" element={<ManageJobs />} />
-        <Route path="/recruiter/managejob/:jobId/analytics" element={<JobAnalytics />} /> 
-        <Route path="/recruiter/createjob" element={<CreateJob />}  />
-        <Route path="/recruiter/applications" element={<JobApplicationList />} />
-        <Route path="/recruiter/job-seekers" element={<RecruiterJobSeekerSearch />} />
-      </Route>
-             
-    </Routes>      
-      <div><Footer/></div>
+        <Route path="/ContactUs" element={<ContactUs />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+        <Route path="/TermsAndConditions" element={<TermsAndConditions />} />
+        <Route path="/Suggestions" element={<Suggestions />} />
+        <Route path="/pricing" element={<Pricing />} />
+
+        <Route path="/jobs/updatejob/:id" element={<EditJob />} />
 
         {/* ADMIN */}
 
@@ -199,13 +144,14 @@ function App() {
           <Route path="applications" element={<JobApplicationList />} />
           <Route path="job-seekers" element={<RecruiterJobSeekerSearch />} />
         </Route>
-      <Footer />
 
+        <Route path="/recruiter/companyrequest" element={<RecruiterCompanyRequest />} />
+
+      </Routes>
+
+      <Footer />
     </>
   )
 }
 
-
-
-export default App;
-
+export default App
--- src/components/jobs/JobCards.jsx
+++ src/components/jobs/JobCards.jsx
@@ -11,12 +11,15 @@ function JobCards({ job, recentlyOpened = false }) {
 
   const handleSaveJob = (e) => {
     e.stopPropagation();
+
     const savedJobs = JSON.parse(localStorage.getItem("savedJobs")) || [];
     const alreadySaved = savedJobs.find((j) => j.id === job.id);
+
     if (alreadySaved) {
       alert("Job already saved");
       return;
     }
+
     savedJobs.push(job);
     localStorage.setItem("savedJobs", JSON.stringify(savedJobs));
     alert("Job saved successfully");
@@ -25,45 +28,82 @@ function JobCards({ job, recentlyOpened = false }) {
   return (
     <div
       onClick={handleClick}
-      className="bg-white rounded-lg shadow-sm hover:shadow-md transition cursor-pointer flex justify-between items-start px-5 py-4 gap-4 w-full min-h-[100px]"
+      className="bg-white rounded-lg shadow-sm hover:shadow-md transition cursor-pointer flex justify-between items-start px-5 py-4 gap-4 w-full"
     >
-      {/* Left side: Job info */}
+      {/* LEFT SIDE */}
       <div className="flex-1 flex flex-col gap-2 overflow-hidden">
-        {/* Title & Company */}
-        <h3 className="text-lg font-semibold text-gray-800 truncate">{job.title}</h3>
-        <p className="text-sm text-gray-600 truncate">{job.company?.companyName}</p>
+
+        {/* Job Title */}
+        <h3 className="text-lg font-semibold text-gray-800 truncate">
+          {job.title}
+        </h3>
+
+        {/* Company Name */}
+        <p className="text-sm text-gray-600 truncate">
+          {job.company?.companyName}
+        </p>
 
         {/* Location + Experience + Salary */}
         <div className="flex flex-wrap gap-4 text-sm text-gray-600 mt-1">
           {job.location && <span>{job.location}</span>}
+
           {job.minExperience !== null && job.maxExperience !== null && (
             <span>{job.minExperience} - {job.maxExperience} yrs</span>
           )}
-          <span>{job.salary ? `₹${job.salary.toLocaleString()} LPA` : "Salary Not Disclosed"}</span>
+
+          <span>
+            {job.salary
+              ? `₹${job.salary.toLocaleString()} LPA`
+              : "Salary Not Disclosed"}
+          </span>
+
+          {job.jobType && <span>{job.jobType}</span>}
         </div>
 
-        {/* Skills / Tags */}
+        {/* Description */}
+        {job.description && (
+          <p className="text-sm text-gray-500 line-clamp-2">
+            {job.description}
+          </p>
+        )}
+
+        {/* Skills Required */}
         {job.skills && job.skills.length > 0 && (
-          <div className="flex flex-wrap gap-2 mt-1">
-            {job.skills.map((skill, index) => (
-              <span
-                key={index}
-                className="text-xs bg-gray-100 text-gray-700 px-3 py-1 rounded-md"
-              >
-                {skill}
-              </span>
-            ))}
+          <div className="mt-2">
+            <p className="text-xs font-medium text-gray-600 mb-1">
+              Skills Required
+            </p>
+
+            <div className="flex flex-wrap gap-2">
+              {job.skills.slice(0, 6).map((skill, index) => (
+                <span
+                  key={index}
+                  className="text-xs bg-gray-100 text-gray-700 px-3 py-1 rounded-md"
+                >
+                  {skill}
+                </span>
+              ))}
+
+              {job.skills.length > 6 && (
+                <span className="text-xs text-gray-500">
+                  +{job.skills.length - 6} more
+                </span>
+              )}
+            </div>
           </div>
         )}
       </div>
 
-      {/* Right side: Recently Opened + Save */}
+      {/* RIGHT SIDE */}
       <div className="flex flex-col justify-between items-end gap-2">
+
         {recentlyOpened && (
-          <span className="text-xs text-blue-600 font-medium">Recently Opened</span>
+          <span className="text-xs text-blue-600 font-medium">
+            Recently Opened
+          </span>
         )}
 
-        {/* Save button with icon + text */}
+        {/* Save Job */}
         <button
           onClick={handleSaveJob}
           className="flex items-center gap-1 p-2 rounded-md hover:bg-gray-100 text-gray-600 text-sm"
@@ -72,7 +112,10 @@ function JobCards({ job, recentlyOpened = false }) {
           <span>Save</span>
         </button>
 
-        <span className="text-xs text-gray-400">{job.createdAt && `Posted ${timeAgo(job.createdAt)}`}</span>
+        {/* Posted Time */}
+        <span className="text-xs text-gray-400">
+          {job.createdAt && `Posted ${timeAgo(job.createdAt)}`}
+        </span>
       </div>
     </div>
   );
